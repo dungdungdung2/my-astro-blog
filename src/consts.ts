298410851b4b07f8c7028aa85d33185ceecf0a85
dungdungdung2 <1food2taste@gmail.com>
2025-07-05 16:51:02 +0000

    Update site metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Koctogame';
+export const SITE_DESCRIPTION = 'Where Your Daily Choices Become Your Paycheck';
+
